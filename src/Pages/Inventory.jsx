cb930aa32540e7dc662cbf273f1395fb71fff388
Lahiru Kavinda <lahiruxk@gmail.com>
2024-12-09 23:42:55 +0530

    update

--- src/Pages/Inventory.jsx
+++ src/Pages/Inventory.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Inventory = () => {
-  return <div>gggggggg</div>;
+  return <>
+  <div className="flex justify-center items-center w-full h-32 bg-[#182527]">
+    <h2 className="text-white font-medium text-center text-base sm:text-lg md:text-2xl lg:text-4xl 2xl:text-6xl italic">Inventory Management</h2>
+  </div>
+  </>;
 };
 
 export default Inventory;

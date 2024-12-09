2d1ae89d703fe1f67de135f19b66b23575cb0d53
Lahiru Kavinda <lahiruxk@gmail.com>
2024-12-09 23:56:15 +0530

    update

--- src/Pages/Inventory.jsx
+++ src/Pages/Inventory.jsx
@@ -1,42 +1,57 @@
 import React from "react";
 
 const Inventory = () => {
-  return <>
-  <div className="flex justify-center items-center w-full h-32 bg-[#182527]">
-    <h2 className="text-white font-medium text-center text-base sm:text-lg md:text-2xl lg:text-4xl 2xl:text-6xl italic">Inventory Management</h2>
-  </div>
+  return (
+    <>
+      <div className="flex justify-center items-center w-full h-32 bg-[#182527]">
+        <h2 className="text-white font-medium text-center text-base sm:text-lg md:text-2xl lg:text-4xl 2xl:text-6xl italic">
+          Inventory Management
+        </h2>
+      </div>
 
-  <table class="min-w-full table-auto border-separate border-spacing-0.5">
-  <thead>
-    <tr class="bg-green-500 text-white">
-      <th class="px-4 py-2 text-left text-sm font-semibold">Item Name</th>
-      <th class="px-4 py-2 text-left text-sm font-semibold">Quantity</th>
-      <th class="px-4 py-2 text-left text-sm font-semibold">Unit</th>
-      <th class="px-4 py-2 text-left text-sm font-semibold">Threshold</th>
-      <th class="px-4 py-2 text-left text-sm font-semibold">Last Updated</th>
-      <th class="px-4 py-2 text-left text-sm font-semibold">Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr class="hover:bg-gray-100">
-      <td class="px-4 py-2">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td class="px-4 py-2">Malcolm Lockyer</td>
-      <td class="px-4 py-2">1961</td>
-    </tr>
-    <tr class="bg-gray-50 hover:bg-gray-100">
-      <td class="px-4 py-2">Witchy Woman</td>
-      <td class="px-4 py-2">The Eagles</td>
-      <td class="px-4 py-2">1972</td>
-    </tr>
-    <tr class="hover:bg-gray-100">
-      <td class="px-4 py-2">Shining Star</td>
-      <td class="px-4 py-2">Earth, Wind, and Fire</td>
-      <td class="px-4 py-2">1975</td>
-    </tr>
-  </tbody>
-</table>
-
-  </>;
+      <table class="min-w-full table-auto border-separate border-spacing-0.5">
+        <thead>
+          <tr class="bg-[#20cd8d] text-white ">
+            <th class="px-4 py-2 text-left text-sm font-semibold">Item Name</th>
+            <th class="px-4 py-2 text-left text-sm font-semibold">Quantity</th>
+            <th class="px-4 py-2 text-left text-sm font-semibold">Unit</th>
+            <th class="px-4 py-2 text-left text-sm font-semibold">Threshold</th>
+            <th class="px-4 py-2 text-left text-sm font-semibold">
+              Last Updated
+            </th>
+            <th class="px-4 py-2 text-left text-sm font-semibold">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="hover:bg-gray-100">
+            <td class="px-4 py-2">Apples</td>
+            <td class="px-4 py-2">Malcolm Lockyer</td>
+            <td class="px-4 py-2">1961</td>
+          </tr>
+          <tr class="bg-gray-50 hover:bg-gray-100">
+            <td class="px-4 py-2">Milk</td>
+            <td class="px-4 py-2">The Eagles</td>
+            <td class="px-4 py-2">1972</td>
+          </tr>
+          <tr class="hover:bg-gray-100">
+            <td class="px-4 py-2">Eggs</td>
+            <td class="px-4 py-2">Earth, Wind, and Fire</td>
+            <td class="px-4 py-2">1975</td>
+          </tr>
+          <tr class="bg-gray-50 hover:bg-gray-100">
+            <td class="px-4 py-2">Banana</td>
+            <td class="px-4 py-2">The Eagles</td>
+            <td class="px-4 py-2">1972</td>
+          </tr>
+          <tr class="hover:bg-gray-100">
+            <td class="px-4 py-2">Orange</td>
+            <td class="px-4 py-2">Earth, Wind, and Fire</td>
+            <td class="px-4 py-2">1975</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Inventory;

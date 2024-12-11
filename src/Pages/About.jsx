96ed1d891371c563fffe8f2ff11931289da24dcd
Lahiru Kavinda <lahiruxk@gmail.com>
2024-12-11 10:34:22 +0530

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,66 @@
 const About = () => {
   return (
     <>
+      {/* Transforming Section */}
       <div className="flex items-center justify-center ">
         <div className="w-5/6 h-fit bg-white/60 shadow-2xl ring-1 ring-gray-900/10 rounded-3xl m-2 md:m-3 xl:m-5 flex flex-col items-center ">
-          <h1 className="text-white font-semibold text-center m-2 p-2 text-xl sm:text-2xl md:text-4xl lg:text-5xl  2xl:text-7xl">Transforming Grocery Shopping</h1>
-          <p className="text-justify font-light w-5/6 m-2 p-2 text-xs sm:text-sm md:text-base lg:text-lg 2xl:text-xl ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis vitae corrupti vel repellendus minus, qui culpa. Minima perferendis vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis vitae corrupti vel repellendus minus, qui culpa. Minima perferendis vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis vitae corrupti vel repellendus minus, qui culpa. Minima perferendis vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis vitae corrupti vel repellendus minus, qui culpa. Minima perferendis vero, maxime at eligendi ratione tenetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis vitae corrupti vel repellendus minus, qui culpa. Minima perferendis vero, maxime at eligendi ratione tenetur Lorem </p>
+          <h1 className="text-white font-semibold text-center m-2 p-2 text-xl sm:text-2xl md:text-4xl lg:text-5xl  2xl:text-7xl">
+            Transforming Grocery Shopping
+          </h1>
+          <p className="text-justify font-light w-5/6 m-2 p-2 text-xs sm:text-sm md:text-base lg:text-lg 2xl:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            unde, quaerat, autem dolores quo veritatis vitae corrupti vel
+            repellendus minus, qui culpa. Minima perferendis vero, maxime at
+            eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis
+            vitae corrupti vel repellendus minus, qui culpa. Minima perferendis
+            vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Maiores unde, quaerat, autem dolores
+            quo veritatis vitae corrupti vel repellendus minus, qui culpa.
+            Minima perferendis vero, maxime at eligendi ratione tenetur? Lorem
+            ipsum dolor sit amet consectetur adipisicing elit. Maiores unde,
+            quaerat, autem dolores quo veritatis vitae corrupti vel repellendus
+            minus, qui culpa. Minima perferendis vero, maxime at eligendi
+            ratione tenetur Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Maiores unde, quaerat, autem dolores quo veritatis vitae
+            corrupti vel repellendus minus, qui culpa. Minima perferendis vero,
+            maxime at eligendi ratione tenetur Lorem{" "}
+          </p>
+        </div>
+      </div>
+
+      {/* Mission & Vision */}
+      <div className="flex items-center justify-center ">
+        <div className="w-5/6 h-fit bg-white/60 shadow-2xl ring-1 ring-gray-900/10 rounded-3xl m-2 md:m-3 xl:m-5 flex flex-col items-center ">
+          <h1 className="text-white font-semibold text-center m-2 p-2 text-xl sm:text-2xl md:text-4xl lg:text-5xl  2xl:text-7xl">
+            Brief description about the platform
+          </h1>
+          <p className="text-justify font-light w-5/6 m-2 p-2 text-xs sm:text-sm md:text-base lg:text-lg 2xl:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            unde, quaerat, autem dolores quo veritatis vitae corrupti vel
+            repellendus minus, qui culpa. Minima perferendis vero, maxime at
+            eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis
+            vitae corrupti vel repellendus minus, qui culpa. Minima perferendis
+            vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Maiores unde, quaerat, autem dolores
+            quo v{" "}
+          </p>
+
+          <h1 className="text-white font-semibold text-center m-2 p-2 text-xl sm:text-2xl md:text-4xl lg:text-5xl  2xl:text-7xl">
+            Mission & Vision
+          </h1>
+          <p className="text-justify font-light w-5/6 m-2 p-2 text-xs sm:text-sm md:text-base lg:text-lg 2xl:text-xl ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            unde, quaerat, autem dolores quo veritatis vitae corrupti vel
+            repellendus minus, qui culpa. Minima perferendis vero, maxime at
+            eligendi ratione tenetur? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Maiores unde, quaerat, autem dolores quo veritatis
+            vitae corrupti vel repellendus minus, qui culpa. Minima perferendis
+            vero, maxime at eligendi ratione tenetur? Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Maiores unde, quaerat, autem dolores
+            quo v{" "}
+          </p>
         </div>
       </div>
     </>

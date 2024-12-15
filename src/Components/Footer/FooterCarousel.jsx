07de4e3282d989cac9740326432bc0287bfca1c5
Lahiru Kavinda <lahiruxk@gmail.com>
2024-12-15 11:08:07 +0530

    Update FooterCarousel.jsx

--- src/Components/Footer/FooterCarousel.jsx
+++ src/Components/Footer/FooterCarousel.jsx
@@ -5,16 +5,46 @@ const FooterCarousel = () => {
   const [speed, setSpeed] = useState(50); // Default speed
 
   const images = [
-    { src: "https://placehold.co/600x400", alt: "CompanyName1" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName2" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName3" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName4" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName5" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName6" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName7" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName8" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName9" },
-    { src: "https://placehold.co/600x400", alt: "CompanyName10" },
+    {
+      src: "https://awsmd.com/_next/static/media/uber.391ab754.svg",
+      alt: "CompanyName1",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/oracle.f6c579b2.svg ",
+      alt: "CompanyName2",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/gofundme.d15a4c9e.svg",
+      alt: "CompanyName3",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/nutanix.76cbb1b9.svg",
+      alt: "CompanyName4",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/upside.3fc33722.svg",
+      alt: "CompanyName5",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/intel.0d9f9c40.svg",
+      alt: "CompanyName6",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/new-balance.9bcd089f.svg",
+      alt: "CompanyName7",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/statefarm.91373514.svg",
+      alt: "CompanyName8",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/crosslead.2cd4c2e3.png",
+      alt: "CompanyName9",
+    },
+    {
+      src: "https://awsmd.com/_next/static/media/voldis.21ec3317.png",
+      alt: "CompanyName10",
+    },
   ];
 
   useEffect(() => {
@@ -63,7 +93,7 @@ const FooterCarousel = () => {
         {images.concat(images).map((image, index) => (
           <img
             key={index}
-            className="w-28 h-auto p-2 border sm:w-32 md:w-40 lg:w-52 xl:w-64 2xl:w-80 max-w-none"
+            className="w-28 h-40 p-16 border sm:w-32 md:w-40 lg:w-52 xl:w-64 2xl:w-80 max-w-none"
             src={image.src}
             alt={image.alt}
           />
